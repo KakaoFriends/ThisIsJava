ad509da4a16600864b6473ed74078dbe89ebd4a3
KakaoFriends <lucky5287@naver.com>
2019-01-15 17:10:44 +0900

    chap06 6.8.3 Calculator의 execute() 실행

---
+++ chap06/src/Calculator.java
@@ -0,0 +1,22 @@
+// 6.8.3 클래스 내부에서 메소드 호출
+public class Calculator {
+    int plus(int x, int y) {
+        int result = x + y;
+        return result;
+    }
+
+    double avg(int x, int y) {
+        double sum = plus(x, y);
+        double result = sum / 2;
+        return result;
+    }
+
+    void execute() {
+        double result = avg(7, 10);
+        println("실행결과: " + result);
+    }
+
+    void println(String message) {
+        System.out.println(message);
+    }
+}
---
+++ chap06/src/CalculatorExample.java
@@ -0,0 +1,7 @@
+// 6.8.3 Calculator의 execute() 실행
+public class CalculatorExample {
+    public static void main(String[] args) {
+        Calculator myCalc = new Calculator();
+        myCalc.execute();
+    }
+}
